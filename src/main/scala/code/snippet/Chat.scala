bacbf0a26523f656ac3193b3d1d3899960e162e0
Joe Barnes <barnesjd@gmail.com>
2016-04-12 20:02:30 -0500

    Converted to ajax form and discovered that the whole progressive thing already works for free

--- src/main/scala/code/snippet/Chat.scala
+++ src/main/scala/code/snippet/Chat.scala
@@ -1,6 +1,8 @@
 package code.snippet
 
-import net.liftweb.http.S
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds.{SetValById, SetElemById}
+import net.liftweb.http.{SHtml, S}
 import net.liftweb.util.ClearClearable
 import net.liftweb.util.Helpers._
 
@@ -11,16 +13,26 @@ object Chat {
 
   def messages = ".chat-message *" #> ms & ClearClearable
 
-  def submit(html:NodeSeq):NodeSeq = {
+  private [this] def append(msg:String):Unit = {
+    ms = (ms :+ msg).takeRight(10)
+  }
 
-    for {
-      r <- S.request if r.post_?
-      msg <- S.param("in")
-    } yield {
-      ms = (ms :+ msg).takeRight(10)
-      S.redirectTo("/")
+  private [this] def doPost:Unit = for {
+    r <- S.request if r.post_?
+    msg <- S.param("in")
+  } yield {
+    append(msg)
+    S.redirectTo("/")
+  }
+
+  def submit = {
+    var msg = ""
+
+    def onAjax():JsCmd = {
+      append(msg)
+      SetValById("chat-in", "")
     }
 
-    html
+    "name=in" #> (SHtml.text(msg, msg = _, "id" -> "chat-in") ++ SHtml.hidden(onAjax))
   }
 }
